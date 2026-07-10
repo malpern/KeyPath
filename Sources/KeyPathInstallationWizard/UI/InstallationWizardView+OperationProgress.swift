e59d4be4e017a08bb2321a2e2cef0afdf9a01d50
Micah Alpern <malpern@gmail.com>
2026-07-10 04:04:21 -0700

    Render wizard from owned run result

--- Sources/KeyPathInstallationWizard/UI/InstallationWizardView+OperationProgress.swift
+++ Sources/KeyPathInstallationWizard/UI/InstallationWizardView+OperationProgress.swift
@@ -17,18 +17,4 @@ public extension InstallationWizardView {
     func isCurrentOperationIndeterminate() -> Bool {
         true
     }
-
-    func getDetailedErrorMessage(for action: AutoFixAction, actionDescription _: String)
-        async -> String
-    {
-        var message = AutoFixActionDescriptions.errorMessage(for: action)
-
-        if action == .restartVirtualHIDDaemon || action == .startKarabinerDaemon {
-            if let detail = await kanataManager?.getVirtualHIDBreakageSummary(), !detail.isEmpty {
-                message += "\n\n" + detail
-            }
-        }
-
-        return message
-    }
 }
